fbda6c367f2004ba83ed18088060950c4861cd08
Vivek D <vivekduraisamy1@gmail.com>
2019-05-16 08:13:01 +0530

    First Commit

---
+++ gitstart/src/com/vivek/util/PrintHello.java
@@ -0,0 +1,12 @@
+package com.vivek.util;
+
+import java.time.LocalDate;
+
+public class PrintHello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Vivek! " + LocalDate.now());
+
+	}
+
+}
